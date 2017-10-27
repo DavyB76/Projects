df99dd0b90a7b93a282f316dad18bda701290788
davybureau <davy.bureau@gmail.com>
2017-10-27 23:18:48 +0200

    adding array operation & listcomp
    Signed-off-by: davybureau <davy.bureau@gmail.com>

--- arrays.py
+++ arrays.py
@@ -7,3 +7,23 @@ def reorderArrayWithEvenFirst(reorderedArray):
         else:
             reorderedArray[next_even], reorderedArray[next_odd] = reorderedArray[next_odd], reorderedArray[next_even]
             next_odd -= 1
+
+
+def transform_string_to_list_using_for(symbols):
+    returnSeq = []
+
+    for symbol in symbols:
+        returnSeq.append(ord(symbol))
+
+    return returnSeq
+
+
+def transform_string_to_list_using_listcomp(symbols):
+    return [ord(symbol) for symbol in symbols]
+
+
+def generate_cartesian_product_list_from_two_lists(color_list, size_list):
+    cartesian_product_list = [(color, size) for color in color_list
+                                            for size in size_list]
+
+    return cartesian_product_list
